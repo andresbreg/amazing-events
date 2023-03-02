af3665e00d115c42cce11fc6a9433eb1e73d6be8
andresbregant <andresbregant@gmail.com>
2023-03-02 14:20:26 -0300

    Code Review

--- scripts/past-events.js
+++ scripts/past-events.js
@@ -1,38 +1,21 @@
-let pastEvents = [];
+const pastEventCardContainer = document.getElementById('past-event-card-container');
 
-for (let i=0; i < allEvents.length; i++) {    
-    
-    if (allEvents[i].date < currentDate) {
-        pastEvents.push(allEvents[i]);
-        pastEvents.sort((x, y) => x.date.localeCompare(y.date));
-        pastEvents.reverse();
-    }
-    else {
-    }
-}
+let pastEvents = allEvents.filter(event => event.date < currentDate);
 
-const pastEventCardContainer = document.querySelector('#past-event-card-container');
-
-let pastEventsCards = createPastEventCard(pastEvents);
-
-pastEventCardContainer.innerHTML = pastEventsCards;
-
-function createPastEventCard(events) {
-  let card = '';
-  for (const event of events) {
-    card += `<div class="card col-10 col-sm-5 col-xl-3">
-                <img src="${event.image}" alt="Event cover" class="card-image">
-                <div class="card-body d-flex flex-column justify-content-between">
-                  <p class="fw-semibold">${event.category}</p>
-                  <h5 class="card-title">${event.name}</h5>
-                  <p class="card-text">${event.description}</p>
-                  <div class="d-flex justify-content-between">
-                    <p class="fst-italic">${event.date}</p>
-                    <p>$${event.price}</p>
-                  </div>
-                  <a href="details.html" class="btn details-btn">Details</a>
-                </div>
-              </div>`
-  }
-  return card;
-}
+pastEvents.sort((x,y) => x.date.localeCompare(y.date));
+pastEvents.reverse();
+pastEvents.forEach((event) => {pastEventCardContainer.innerHTML +=
+  `<div class="card col-10 col-sm-5 col-xl-3">
+    <img src="${event.image}" alt="Event cover" class="card-image">
+    <div class="card-body d-flex flex-column justify-content-between">
+      <p class="fw-semibold">${event.category}</p>
+      <h5 class="card-title">${event.name}</h5>
+      <p class="card-text">${event.description}</p>
+      <div class="d-flex justify-content-between">
+        <p class="fst-italic">${event.date}</p>
+        <p>$${event.price}</p>
+      </div>
+      <a href="details.html" class="btn details-btn">Details</a>
+    </div>
+  </div>`
+});
--- scripts/upcoming-events.js
+++ scripts/upcoming-events.js
@@ -1,37 +1,20 @@
-let upcomingEvents = [];
+const upcomingEventCardContainer = document.getElementById('upcoming-event-card-container');
 
-for (let i=0; i < allEvents.length; i++) {    
-    
-    if (allEvents[i].date > currentDate) {
-        upcomingEvents.push(allEvents[i]);
-        upcomingEvents.sort((x, y) => x.date.localeCompare(y.date));
-    }
-    else {        
-    }
-}
+let upcomingEvents = allEvents.filter(event => event.date > currentDate);
 
-const upcomingEventCardContainer = document.querySelector('#upcoming-event-card-container');
-
-let upcomingEventCards = createUpcomingEventCard(upcomingEvents);
-
-upcomingEventCardContainer.innerHTML = upcomingEventCards;
-
-function createUpcomingEventCard(events) {
-  let card = '';
-  for (const event of events) {
-    card += `<div class="card col-10 col-sm-5 col-xl-3">
-                <img src="${event.image}" alt="Event cover" class="card-image">
-                <div class="card-body d-flex flex-column justify-content-between">
-                  <p class="fw-semibold">${event.category}</p>
-                  <h5 class="card-title">${event.name}</h5>
-                  <p class="card-text">${event.description}</p>
-                  <div class="d-flex justify-content-between">
-                    <p class="fst-italic">${event.date}</p>
-                    <p>$${event.price}</p>
-                  </div>
-                  <a href="details.html" class="btn details-btn">Details</a>
-                </div>
-              </div>`
-  }
-  return card;
-}
+upcomingEvents.sort((x,y) => x.date.localeCompare(y.date));
+upcomingEvents.forEach((event) => {upcomingEventCardContainer.innerHTML +=
+  `<div class="card col-10 col-sm-5 col-xl-3">
+    <img src="${event.image}" alt="Event cover" class="card-image">
+    <div class="card-body d-flex flex-column justify-content-between">
+      <p class="fw-semibold">${event.category}</p>
+      <h5 class="card-title">${event.name}</h5>
+      <p class="card-text">${event.description}</p>
+      <div class="d-flex justify-content-between">
+        <p class="fst-italic">${event.date}</p>
+        <p>$${event.price}</p>
+      </div>
+      <a href="details.html" class="btn details-btn">Details</a>
+    </div>
+  </div>`
+});
